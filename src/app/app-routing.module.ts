94302c5806476d8a4be179d9845aafa1289083ac
islamElbadawy <islam.201211@gmail.com>
2024-02-29 18:31:57 +0200

    add social media icons in footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './homepage/components/home/home.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'news',
-    loadChildren: () => import('./news/news.module').then((m) => m.NewsModule),
-  },
-];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
