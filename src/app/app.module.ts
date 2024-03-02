7afc56f5a76a7ab85621e70923ac2074583c0c65
islamElbadawy <islam.201211@gmail.com>
2024-03-02 16:25:29 +0200

    fix importing all ngx-bootstrap icons and just import needed ones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import {
+  NgxBootstrapIconsModule,
+  calendar4,
+  envelope,
+  facebook,
+  instagram,
+  list,
+  shareFill,
+  star,
+  starFill,
+  twitter,
+} from 'ngx-bootstrap-icons';
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
+
+const icons = {
+  list,
+  calendar4,
+  starFill,
+  star,
+  shareFill,
+  facebook,
+  twitter,
+  envelope,
+  instagram,
+};
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxBootstrapIconsModule.pick(allIcons),
+    NgxBootstrapIconsModule.pick(icons),
     HttpClientModule,
     DatePipe,
   ],
